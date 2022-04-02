aebf3c253f558672d6d3ac0823fb9a25746e54f8
Mauricio Buschinelli <mauri.andres@gmail.com>
2022-04-02 14:41:14 -0400

    add dedicated shift keys

--- keymap.c
+++ keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | M_ESC|LGUI_A|LALT_S|LCTL_D|LSFT_F|   G  |-------.    ,-------|   H  |RSFT_J|RCTL_K|RALT_L|RGI_SC|  '   |
  * |------+------+------+------+------+------| MUTE  |    |   XX  |------+------+------+------+------+------|
- * |LCTR  |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RCTL|
+ * | LSFT |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  | RSFT |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | /Enter  /       \Space \  |RAISE | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,     KC_7,    KC_8,     KC_9,    KC_0,   KC_MINS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,     KC_U,    KC_I,     KC_O,    KC_P,   KC_EQL,
   M_ESC,    LGUI_A, LALT_S,  LCTL_D,  LSFT_F,  KC_G,                     KC_H,     RSFT_J,  RCTL_K,   RALT_L,  RGI_SC, KC_QUOT,
-  KC_LCTL,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,      XX,  KC_N,     KC_M,    KC_COMMA, KC_DOT, KC_SLSH,  KC_RCTL,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_MUTE,      XX,  KC_N,     KC_M,    KC_COMMA, KC_DOT, KC_SLSH, KC_RSFT,
                  KC_LGUI, KC_LALT, KC_LCTRL, KC_LOWER, KC_ENT,   KC_SPC, KC_RAISE, KC_RCTRL,KC_RALT,  KC_RGUI
 ),
 /* LOWER
